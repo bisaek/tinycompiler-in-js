aaf71b4cd614ebce0c26ccfc34cd9dfaf8835021
bisaek <huggerbertram@gmail.com>
2023-06-03 16:53:44 +0200

    craete tokenizer

---
+++ compiler.js
@@ -0,0 +1,19 @@
+import tokenizer from "./tokenizer.js";
+import parser from "./parser.js";
+
+export default function compiler(input) {
+  // 1. Lexical Analysis
+	const token = tokenizer(input)
+
+	// 2. Syntactic Analysis
+
+	const lispAST = parser(tokens)
+
+  // 3. Transformation
+
+  // 4. Code Generation
+
+  //
+
+  return lispAST;
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+import compiler from "./compiler.js";
+
+const input = "(add 2 (sub 4 3))"
+const output = compiler(input)
+
+console.log(JSON.stringify(output, null, 2))
